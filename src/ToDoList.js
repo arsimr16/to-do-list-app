517aedd599c8ff7325b6cb025b54e96a25b125e2
arsimr16 <arsimr16@g.holycross.edu>
2018-01-31 16:37:03 -0800

    manage list items in state and create renderList function

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -5,15 +5,23 @@ import ListItem from './ListItem.js';
 class ToDoList extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			items: ['take shower', 'do laundry', 'make dinner']
+		}
+		this.renderList = this.renderList.bind(this);
+	}
+
+	renderList() {
+		return this.state.items.map(item => (
+			<ListItem key={item} item={item}/>
+		));
 	}
 
 	render() {
 		return (
 			<div className="ToDoList">
 				To do List
-				<ListItem item="take shower"/>
-				<ListItem item="do laundry"/>
-				<ListItem item="make dinner"/>
+				{this.renderList()}
 			</div>
 		);
 	}
